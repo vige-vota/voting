7bdea8088b370e9c0fe4be1d9ca0832ea2ed1d19
flashboss <luca.stancapiano@vige.it>
2019-07-18 10:14:04 +0200

    validation

--- src/main/java/it/vige/labs/gc/rest/Validator.java
+++ src/main/java/it/vige/labs/gc/rest/Validator.java
@@ -1,6 +1,7 @@
 package it.vige.labs.gc.rest;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,10 @@ import it.vige.labs.gc.result.Message;
 import it.vige.labs.gc.result.Messages;
 import it.vige.labs.gc.result.Severity;
 import it.vige.labs.gc.vote.Vote;
+import it.vige.labs.gc.vote.VotingPaper;
+import it.vige.labs.gc.votingpapers.Candidate;
+import it.vige.labs.gc.votingpapers.Group;
+import it.vige.labs.gc.votingpapers.Party;
 import it.vige.labs.gc.votingpapers.VotingPapers;
 
 @Component
@@ -38,17 +43,67 @@ public class Validator {
 	public final static Messages defaultMessage = new Messages(true,
 			Arrays.asList(new Message[] { new Message(Severity.message, ok, "all is ok") }));
 
+	public final static Messages errorMessage = new Messages(false,
+			Arrays.asList(new Message[] { new Message(Severity.error, "Generic error", "Validation not ok") }));
+
 	private VotingPapers votingPapers;
 
 	public Messages validate(Vote vote) {
 		VotingPapers votingPapers = getVotingPapers();
-		if (votingPapers.getVotingPapers().parallelStream()
-				.filter(e -> e.getId() == vote.getVotingPapers().get(0).getId()).collect(Collectors.toList())
-				.size() > 0)
-			return defaultMessage;
-		else
-			return new Messages(false,
-					Arrays.asList(new Message[] { new Message(Severity.error, "Id not ok", "Id doesn't exist") }));
+		Boolean[] results = new Boolean[vote.getVotingPapers().size()];
+		for (int i = 0; i < results.length; i++)
+			results[i] = false;
+		int i = 0;
+		for (VotingPaper votingPaper : vote.getVotingPapers()) {
+			it.vige.labs.gc.votingpapers.VotingPaper votingPaperFromJson = votingPapers.getVotingPapers()
+					.parallelStream().filter(e -> e.getId() == votingPaper.getId()).collect(Collectors.toList()).get(0);
+			if (votingPaper.getId() == votingPaperFromJson.getId()) {
+				List<Group> groups = votingPapers.getVotingPapers().parallelStream()
+						.filter(e -> e.getId() == votingPaper.getId()).collect(Collectors.toList()).get(0).getGroups();
+
+				List<Party> parties = groups.get(0).getParties();
+				if (votingPaper.getGroup() != null) {
+					if (!votingPaperFromJson.isDisjointed()) {
+						List<Group> matchedGroups = groups.parallelStream()
+								.filter(e -> e.getId() == votingPaper.getGroup().getId()).collect(Collectors.toList());
+						if (!matchedGroups.isEmpty())
+							parties = groups.parallelStream().filter(e -> e.getId() == votingPaper.getGroup().getId())
+									.collect(Collectors.toList()).get(0).getParties();
+						else
+							results[i] = false;
+					} else
+						parties = groups.parallelStream().flatMap(e -> e.getParties().parallelStream())
+								.collect(Collectors.toList());
+					if (groups.parallelStream().anyMatch(e -> e.getId() == votingPaper.getGroup().getId())
+							&& validateParty(parties, votingPaper.getParty(), votingPaperFromJson.getMaxCandidates()))
+						results[i] = true;
+				} else if (validateParty(parties, votingPaper.getParty(), votingPaperFromJson.getMaxCandidates()))
+					results[i] = true;
+			}
+			i++;
+		}
+		return Arrays.stream(results).allMatch(e -> e == true) ? defaultMessage : errorMessage;
+	}
+
+	private boolean validateParty(List<Party> parties, it.vige.labs.gc.vote.Party party, int maxCandidates) {
+		boolean result = parties.parallelStream().anyMatch(e -> e.getId() == party.getId());
+		if (result && party.getCandidates() != null && !party.getCandidates().isEmpty()) {
+			if (party.getCandidates().size() <= maxCandidates) {
+				List<Candidate> candidates = parties.parallelStream().filter(e -> e.getId() == party.getId())
+						.flatMap(e -> e.getCandidates().parallelStream()).collect(Collectors.toList());
+				result = party.getCandidates().parallelStream()
+						.allMatch(e -> candidates.parallelStream().anyMatch(f -> e.getId() == f.getId()));
+				if (party.getCandidates().size() != 1) {
+					List<Character> sexCandidates = candidates.parallelStream()
+							.filter(e -> party.getCandidates().parallelStream().anyMatch(f -> f.getId() == e.getId()))
+							.map(f -> f.getSex()).collect(Collectors.toList());
+					result = sexCandidates.toString().matches("^(?=.*M)(?=.*F).+$");
+				} else
+					result = true;
+			} else
+				result = false;
+		}
+		return result;
 	}
 
 	private VotingPapers getVotingPapers() {
--- src/test/java/it/vige/labs/gc/VoteTest.java
+++ src/test/java/it/vige/labs/gc/VoteTest.java
@@ -1,6 +1,6 @@
 package it.vige.labs.gc;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Assert;
@@ -25,16 +25,16 @@ import it.vige.labs.gc.vote.VotingPaper;
 @SpringBootTest
 public class VoteTest {
 
-	Logger logger = LoggerFactory.getLogger(VoteTest.class);
+	private Logger logger = LoggerFactory.getLogger(VoteTest.class);
 
 	@Autowired
 	private VoteController voteController;
 
 	@Test
-	public void vote() throws IOException {
+	public void voteOk() {
 
 		Party pd = new Party(3);
-		Group michelBarbet = new Group(6);
+		Group michelBarbet = new Group(5);
 		VotingPaper comunali = new VotingPaper(0, pd, michelBarbet);
 
 		Candidate paolaTaverna = new Candidate(31);
@@ -56,7 +56,88 @@ public class VoteTest {
 
 		Vote vote = new Vote(Arrays.asList(new VotingPaper[] { comunali, regionali, nazionali, europee }));
 		Messages messages = voteController.vote(vote);
+		logger.info(messages + "");
 		Assert.assertArrayEquals("the result is ok", Validator.defaultMessage.getMessages().toArray(),
+				messages.getMessages().toArray());
+		Assert.assertTrue(messages.isOk());
+	}
+
+	@Test
+	public void ids() {
+
+		Party giorgiaMeloni = new Party(93);
+		Group matteoSalvini = new Group(95);
+		VotingPaper nazionali = new VotingPaper(86, giorgiaMeloni, matteoSalvini);
+
+		Vote vote = new Vote(Arrays.asList(new VotingPaper[] { nazionali }));
+		Messages messages = voteController.vote(vote);
+		logger.info(messages + "");
+		Assert.assertArrayEquals("the ids are inverted",
+				Validator.errorMessage.getMessages().toArray(), messages.getMessages().toArray());
+		Assert.assertFalse(messages.isOk());
+		
+		Party noiConSalvini = new Party(94);
+		matteoSalvini.setId(1122);
+		nazionali.setParty(noiConSalvini);
+		messages = voteController.vote(vote);
+		logger.info(messages + "");
+		Assert.assertArrayEquals("the id doesn't exist",
+				Validator.errorMessage.getMessages().toArray(), messages.getMessages().toArray());
+		Assert.assertFalse(messages.isOk());
+	}
+
+	@Test
+	public void disjointed() {
+
+		Party giorgiaMeloni = new Party(95);
+		Group matteoSalvini = new Group(93);
+		VotingPaper nazionali = new VotingPaper(86, giorgiaMeloni, matteoSalvini);
+
+		Vote vote = new Vote(Arrays.asList(new VotingPaper[] { nazionali }));
+		Messages messages = voteController.vote(vote);
+		logger.info(messages + "");
+		Assert.assertArrayEquals("the vote is not disjointed, you can select only the group of the party",
+				Validator.errorMessage.getMessages().toArray(), messages.getMessages().toArray());
+		Assert.assertFalse(messages.isOk());
+	}
+
+	@Test
+	public void candidates() {
+
+		Candidate giulianoSantoboni = new Candidate(30);
+		Candidate matteoCastorino = new Candidate(32);
+		Party movimento5Stelle = new Party(28,
+				new ArrayList<Candidate>(Arrays.asList(new Candidate[] { giulianoSantoboni, matteoCastorino })));
+		Group forzaItalia = new Group(12);
+		VotingPaper regionali = new VotingPaper(11, movimento5Stelle, forzaItalia);
+
+		Vote vote = new Vote(Arrays.asList(new VotingPaper[] { regionali }));
+		Messages messages = voteController.vote(vote);
+		logger.info(messages + "");
+		Assert.assertArrayEquals("there are two M and no F", Validator.errorMessage.getMessages().toArray(),
+				messages.getMessages().toArray());
+		Assert.assertFalse(messages.isOk());
+
+		movimento5Stelle.getCandidates().remove(0);
+		messages = voteController.vote(vote);
+		logger.info(messages + "");
+		Assert.assertArrayEquals("there is only a candidate, it's ok", Validator.defaultMessage.getMessages().toArray(),
+				messages.getMessages().toArray());
+		Assert.assertTrue(messages.isOk());
+
+		Candidate paolaTaverna = new Candidate(31);
+		movimento5Stelle.getCandidates().add(giulianoSantoboni);
+		movimento5Stelle.getCandidates().add(paolaTaverna);
+		messages = voteController.vote(vote);
+		logger.info(messages + "");
+		Assert.assertArrayEquals("candidates number exceed the max candidates value",
+				Validator.errorMessage.getMessages().toArray(), messages.getMessages().toArray());
+		Assert.assertFalse(messages.isOk());
+
+		movimento5Stelle.getCandidates().clear();
+		messages = voteController.vote(vote);
+		logger.info(messages + "");
+		Assert.assertArrayEquals("no candidates, the result is ok", Validator.defaultMessage.getMessages().toArray(),
 				messages.getMessages().toArray());
 		Assert.assertTrue(messages.isOk());
 	}
