ddae103d966ce5c3c961cbfbc825bc2399438965
flashboss <luca.stancapiano@vige.it>
2019-08-13 18:17:33 +0200

    votes and electors

---
+++ src/main/java/it/vige/labs/gc/bean/result/Votes.java
@@ -0,0 +1,17 @@
+package it.vige.labs.gc.bean.result;
+
+import it.vige.labs.gc.bean.vote.Identifier;
+
+public class Votes extends Identifier {
+
+	private int votes;
+
+	public int getVotes() {
+		return votes;
+	}
+
+	public void setVotes(int votes) {
+		this.votes = votes;
+	}
+
+}
